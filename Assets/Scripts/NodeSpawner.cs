092582934d88a0d471347802fd7d29fc6bb34dc7
Hunter Bobeck <hdbipod@gmail.com>
2017-05-03 18:33:21 -0400

    buggy attempt at node spawning from edge drawing only

--- Assets/Scripts/NodeSpawner.cs
+++ Assets/Scripts/NodeSpawner.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// spawns a node at this controller's position whenever the touchpad is pressing, so long as the controller is not currently colliding with a node //
+// for the first node only (requires the nodes container to be empty): spawns a node at this controller's position whenever the touchpad is pressing, so long as the controller is not currently colliding with a node //
 public class NodeSpawner : MonoBehaviour
 {
     public GameObject nodeTemplate;     // connection: node template
@@ -11,20 +11,23 @@ public class NodeSpawner : MonoBehaviour
 
     void Update()
     {
-        if (GetComponent<Controller>().touchpad_pressing())     // if this controller's touchpad is pressing
+        if (nodesContainer.childCount == 0)     // if there are no nodes currently
         {
-            if (NodeDetector.closestNodeInRange(transform.position) == null)      // if there is not a node within a node's radius of the controller's position
+            if (GetComponent<Controller>().touchpad_pressing())     // if this controller's touchpad is pressing
             {
-                // spawn a node called "Node" at this controller's position, parented to the nodes container //
-                GameObject node = Instantiate(nodeTemplate) as GameObject;
-                node.name = "Node";
-                node.transform.parent = nodesContainer;
-                node.transform.position = transform.position;
-            }
-            // otherwise, /*log that there is already a node in this node radius*/ //
-            else      // (there is a node within a node's radius of the controller's position) //
-            {
-                /*Debug.Log("there is already a node in this node radius");*/
+                if (NodeDetector.closestNodeInRange(transform.position) == null)      // if there is not a node within a node's radius of the controller's position
+                {
+                    // spawn a node called "Node" at this controller's position, parented to the nodes container //
+                    GameObject node = Instantiate(nodeTemplate) as GameObject;
+                    node.name = "Node";
+                    node.transform.parent = nodesContainer;
+                    node.transform.position = transform.position;
+                }
+                // otherwise, /*log that there is already a node in this node radius*/ //
+                else      // (there is a node within a node's radius of the controller's position) //
+                {
+                    /*Debug.Log("there is already a node in this node radius");*/
+                }
             }
         }
     }
