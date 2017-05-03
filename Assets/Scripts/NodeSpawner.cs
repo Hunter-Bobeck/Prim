20506475e6e90b10d6cf673b0e3aee968e1f8395
Hunter Bobeck <hdbipod@gmail.com>
2017-05-03 19:05:43 -0400

    Revert "alternative buggy attempt at node spawning from edge drawing only"
    This reverts commit dc4d71d7a93da5cf0bf816f99471bf6569823d07.

--- Assets/Scripts/NodeSpawner.cs
+++ Assets/Scripts/NodeSpawner.cs
@@ -8,27 +8,23 @@ public class NodeSpawner : MonoBehaviour
     public GameObject nodeTemplate;     // connection: node template
 
     public Transform nodesContainer;       // connection: nodes container - transform
-    
-    public GameObject spawnNode()
-    {
-        // spawn a node called "Node" at this controller's position, parented to the nodes container //
-        GameObject node = Instantiate(nodeTemplate) as GameObject;
-        node.name = "Node";
-        node.transform.parent = nodesContainer;
-        node.transform.position = transform.position;
-        return node;
-    }
 
     void Update()
     {
-        if (nodesContainer.childCount == 0)
+        if (GetComponent<Controller>().touchpad_pressing())     // if this controller's touchpad is pressing
         {
-            if (GetComponent<Controller>().touchpad_pressing())     // if this controller's touchpad is pressing
+            if (NodeDetector.closestNodeInRange(transform.position) == null)      // if there is not a node within a node's radius of the controller's position
             {
-                if (NodeDetector.closestNodeInRange(transform.position) == null)      // if there is not a node within a node's radius of the controller's position
-                {
-                    spawnNode();
-                }
+                // spawn a node called "Node" at this controller's position, parented to the nodes container //
+                GameObject node = Instantiate(nodeTemplate) as GameObject;
+                node.name = "Node";
+                node.transform.parent = nodesContainer;
+                node.transform.position = transform.position;
+            }
+            // otherwise, /*log that there is already a node in this node radius*/ //
+            else      // (there is a node within a node's radius of the controller's position) //
+            {
+                /*Debug.Log("there is already a node in this node radius");*/
             }
         }
     }
